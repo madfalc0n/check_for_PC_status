7cdec78c34ea31585d0288ef92a25c8980b6c13c
madfalc0n <chadool116@naver.com>
2021-04-06 17:58:31 +0900

    Add : TelegramBot

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from modules.CheckPowerStatus import get_power_status
 from modules.PostToSlack import post_slack
+from modules.test_telegram import telegram_bot
 import time
 
-def main_process():
+def main_process(mode=1):
+    """
+    mode 0 : Slack communication
+    mode 1 : Telegram communication
+    """
     count = 1
     while 1:
         if count == 5:
@@ -19,12 +24,19 @@ def main_process():
             window_status = 'OFFLINE'
         else:
             window_status = 'ONLINE'
-
-        url = 'YOUR SLACK WORKSPACE URL'
+        
         text_form = f"POWER Line Status : {Battery_connected} \nWindow Locked : {window_status}"
+        
 
-        slack_post_result = post_slack(url,text_form)
-        print(slack_post_result)
+        if mode == 0: ##Slack 
+            url = 'YOUR SLACK WORKSPACE URL'
+            slack_post_result = post_slack(url,text_form)
+            print(slack_post_result)
+        elif mode == 1: ##Telegram
+            token = 'YOUR TELEGRAM BOT TOKEN'
+            tb = telegram_bot(token)
+            tb.post_telegram(text_form)
+
         count += 1
         time.sleep(40)
 
@@ -33,4 +45,4 @@ if __name__ == "__main__":
     print("----------------------------")
     print("CHECK YOUR PC STATUS")
     print("----------------------------")
-    main_process()
+    main_process(mode=1)
